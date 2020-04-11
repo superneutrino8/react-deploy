d1ca50b77cab3bea6299f0194125f7dd920ce937
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-04-11 16:04:21 +0530

    added css class to header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App App-header">
       <h1>Hello React</h1>
       <h2>Testing React Deploying using GitHub Pages</h2>
       <p>Author: Vivek Bariya</p>
